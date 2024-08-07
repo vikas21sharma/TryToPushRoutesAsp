8431ed25623b458a0b6f387b83377a4cc5f58d9d
vikas21sharma <vikashsh333@gmail.com>
2024-08-07 22:48:33 +0530

    origin

---
+++ routesASPApplication/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace routesASPApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/date")]
+        public int date(int id)
+        {
+            return id;
+        }
+    }
+}
